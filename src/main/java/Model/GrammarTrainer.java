e6257f1d7422cf9205736e3be0300e9d95cefcb0
Eric Dreier <edreier@student.tgm.ac.at>
2023-10-04 14:36:12 +0200

    controller

--- src/main/java/Model/GrammarTrainer.java
+++ src/main/java/Model/GrammarTrainer.java
@@ -1,5 +1,6 @@
 package Model;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class GrammarTrainer {
@@ -12,6 +13,38 @@ public class GrammarTrainer {
 
 	private String input;
 
-	private Wordpair[] wordpair;
+	public GrammarTrainer() {
+		this.pair = new LinkedList<>();
+	}
+
+	public int getRight() {
+		return this.right;
+	}
+
+	public int getWrong() {
+		return this.wrong;
+	}
+
+	public void addWordpair(Wordpair pair) {
+		if(pair!=null)	{
+			this.pair.add(pair);
+		} else {
+			throw new IllegalArgumentException("Wordpair is null");
+		}
+	}
+
+	public Wordpair getWordpair(int index) {
+		if(index <= pair.size()-1)	{
+			return this.pair.get(index);
+		} else {
+			throw new IllegalArgumentException("Index doesn't exist");
+		}
+	}
+
+	public void setInput(String input) {
+		if(!input.equals(null))	{
+			this.input = input;
+		}
+	}
 
 }
--- src/main/java/Model/Wordpair.java
+++ src/main/java/Model/Wordpair.java
@@ -9,31 +9,26 @@ public class Wordpair {
 	private URL url;
 
 	public Wordpair(String word, URL url) {
-
+		this.url = url;
+		this.word = word;
 	}
 
 	public void setWord(String word) {
-
+		if(!word.equals(null) && !word.equals("")){
+			this.word = word;
+		}
 	}
 
 	public void setURL(URL url) {
-
+		this.url = url;
 	}
 
 	public String getWord() {
-		return null;
+		return this.word;
 	}
 
 	public URL getURL() {
-		return null;
-	}
-
-	public boolean checkURL() {
-		return false;
-	}
-
-	public boolean checkWord() {
-		return false;
+		return this.url;
 	}
 
 }
