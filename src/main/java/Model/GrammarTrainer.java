71390ef13b5403e699fbb8e289409eab8fa122ad
Eric Dreier <edreier@student.tgm.ac.at>
2023-10-04 14:17:10 +0200

    controller concept

---
+++ src/main/java/Model/GrammarTrainer.java
@@ -0,0 +1,17 @@
+package Model;
+
+import java.util.List;
+
+public class GrammarTrainer {
+
+	private List<Wordpair> pair;
+
+	private int wrong;
+
+	private int right;
+
+	private String input;
+
+	private Wordpair[] wordpair;
+
+}
---
+++ src/main/java/Model/Wordpair.java
@@ -0,0 +1,39 @@
+package Model;
+
+import java.net.URL;
+
+public class Wordpair {
+
+	private String word;
+
+	private URL url;
+
+	public Wordpair(String word, URL url) {
+
+	}
+
+	public void setWord(String word) {
+
+	}
+
+	public void setURL(URL url) {
+
+	}
+
+	public String getWord() {
+		return null;
+	}
+
+	public URL getURL() {
+		return null;
+	}
+
+	public boolean checkURL() {
+		return false;
+	}
+
+	public boolean checkWord() {
+		return false;
+	}
+
+}
